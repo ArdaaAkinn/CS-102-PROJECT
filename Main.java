08741f8715143b63f93e930e739e85df00e6940b
derya polat <derya.polat@ug.bilkent.edu.tr>
2025-02-05 18:02:27 +0300

    Fixed.

--- Main.java
+++ Main.java
@@ -1,1 +1,48 @@
+public class Main {
 
+    public static void displayMenu()
+    {
+        Scanner scan = new Scanner (System.in);
+        boolean exit = false;
+        while(!exit)
+        {   
+            System.out.println("Menu:");
+            System.out.println("1. Display the minimum of the array.");
+            System.out.println("2. Display the maximum of the array.");
+            System.out.println("3. Display how each element differs from the average.");
+            System.out.println("4. Display the sum of elements with odd and even indexes.");
+            System.out.println("5. Exit.");
+            System.out.println("Choose an option: ");
+
+            int choice = scan.nextInt();
+            scan.nextLine();
+
+            switch(choice)
+            {
+                case 1:
+                    //minimum method
+                    break;
+                case 2:
+                    //maximum method
+                    break;
+                case 3:
+                    //differs from average method
+                    break;
+                case 4:
+                    //sum odd-even method
+                    break;
+                case 5:
+                    exit=true;
+                    System.out.println("Exiting the menu.");
+                    scan.close();
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        displayMenu();
+    }
+}

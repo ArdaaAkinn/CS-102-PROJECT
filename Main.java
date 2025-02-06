0cfa0e35dd2ca42f95f75ba4fecb59bff26a3964
derya polat <derya.polat@ug.bilkent.edu.tr>
2025-02-06 21:57:32 +0300

    Fixed some problems on my part.
    I pulled the "scanner" object to the main method. Opened a do while loop on the main method instead of the displayMenu() method.

--- Main.java
+++ Main.java
@@ -19,49 +19,49 @@ public class Main {
     }
 
     public static void displayMenu()
-    {
-        Scanner scan = new Scanner (System.in);
-        boolean exit = false;
-        while(!exit)
-        {   
-            System.out.println("Menu:");
-            System.out.println("1. Display the minimum of the array.");
-            System.out.println("2. Display the maximum of the array.");
-            System.out.println("3. Display how each element differs from the average.");
-            System.out.println("4. Display the sum of elements with odd and even indexes.");
-            System.out.println("5. Exit.");
-            System.out.println("Choose an option: ");
-
-            int choice = scan.nextInt();
-            scan.nextLine();
-
-            switch(choice)
-            {
-                case 1:
-                    //minimum method
-                    break;
-                case 2:
-                    //maximum method
-                    break;
-                case 3:
-                    //differs from average method
-                    break;
-                case 4:
-                    sumOfEvenIndexes(null); // write the array's name instead of null.
-                    sumOfOddIndexes(null); // write the array's name instead of null.
-                    break;
-                case 5:
-                    exit=true;
-                    System.out.println("Exiting the menu.");
-                    scan.close();
-                    break;
-                default:
-                    System.out.println("Invalid option.");
-            }
-        }
+    {  
+        System.out.println("Menu:");
+        System.out.println("1. Display the minimum of the array.");
+        System.out.println("2. Display the maximum of the array.");
+        System.out.println("3. Display how each element differs from the average.");
+        System.out.println("4. Display the sum of elements with odd and even indexes.");
+        System.out.println("5. Exit.");
+        System.out.println("Choose an option: ");
     }
 
     public static void main(String[] args){
-        displayMenu();
+        
+        Scanner scan = new Scanner (System.in);
+        boolean exit = false;
+        do
+        {
+            displayMenu();
+            int choice = scan.nextInt();
+            scan.nextLine();
+            switch(choice)
+                {
+                    case 1:
+                        //minimum method
+                        break;
+                    case 2:
+                        //maximum method
+                        break;
+                    case 3:
+                        //differs from average method
+                        break;
+                    case 4:
+                        sumOfEvenIndexes(null); // write the array's name instead of null.
+                        sumOfOddIndexes(null); // write the array's name instead of null.
+                        break;
+                    case 5:
+                        exit=true;
+                        System.out.println("Exiting the menu.");
+                        scan.close();
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                }
+            }
+            while(!exit);
     }
 }
